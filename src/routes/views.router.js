40a37659c1ad4c4b29facc6b43f90fc856d02ec9
guillejf <maildeguille@gmail.com>
2023-06-13 23:57:10 -0300

    final version

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,52 +1,125 @@
-const express = require('express');
+const express = require("express");
 const viewsRouter = express.Router();
-const ProductService = require('../services/products.service.js');
+const ProductService = require("../services/products.service.js");
 const productService = new ProductService();
-const CartService = require('../services/carts.service.js');
+const CartService = require("../services/carts.service.js");
+const { ProductModel } = require("../DAO/models/products.model.js");
 const cartService = new CartService();
 
-viewsRouter.get('/', async (req, res) => {
-    try{
-        const { limit = 10, page = 1, sort, query } = req.query;
-        const queryParams = { limit, page, sort, query };
-        const products = await productService.get(queryParams);
-        console.log(products); //BORRAR
-        return res.status(200).render('home', {products});
-    }catch(err){
-        console.error(err);
-        return res.status(500).json({status: "error", msg: "Error in server", products:{}})
-    }
+viewsRouter.get("/", async (req, res) => {
+  try {
+    const { limit = 10, page = 1, sort, query } = req.query;
+    const queryParams = { limit, page, sort, query };
+    const products = await productService.get(queryParams);
+    console.log(products); //BORRAR
+    return res.status(200).render("home", { products });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ status: "error", msg: "Error in server", products: {} });
+  }
 });
 
-viewsRouter.get('/realtimeproducts', async (req, res) => {
-    try{
-        const products = await productService.get();
-        return res.status(200).render('realTimeProducts', {products});
-    }catch(err){
-        return res.status(500).json({status: "error", msg: "Error in server", products:{}})
-    }
+viewsRouter.get("/realtimeproducts", async (req, res) => {
+  try {
+    const products = await productService.get();
+    return res.status(200).render("realTimeProducts", { products });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ status: "error", msg: "Error in server", products: {} });
+  }
 });
 
-viewsRouter.get('/products', async (req, res) => {
-    try {
-        const {limit = 10, page = 1, sort, query} = req.query;
-        const queryParams = {limit, page, sort, query};
-        
-        const {payload: products, totalPages} = await productService.get(queryParams);
-        return res.render('products', { products, totalPages });
-    } catch (error) {
-        return res.status(500).json({status: 'error', message: 'Error in server'});
-    }
+viewsRouter.get("/products", async (req, res) => {
+  try {
+    const { limit = 10, page = 1, sort, query } = req.query;
+    const queryParams = { limit, page, sort, query };
+
+    const {
+      payload: products,
+      totalPages,
+      payload,
+      prevPage,
+      nextPage,
+      page: currentPage,
+      hasPrevPage,
+      hasNextPage,
+      prevLink,
+      nextLink,
+    } = await productService.get(queryParams);
+    let productsSimplified = products.map((item) => {
+      return {
+        _id: item._id.toString(),
+        title: item.title,
+        description: item.description,
+        price: item.price,
+        thumbnail: item.thumbnail,
+        code: item.code,
+        stock: item.stock,
+        category: item.category,
+      };
+    });
+
+    return res.render("products", {
+      products: productsSimplified,
+      totalPages,
+      prevPage,
+      nextPage,
+      currentPage,
+      hasPrevPage,
+      hasNextPage,
+      prevLink: prevLink?.substring(4) || "",
+      nextLink: nextLink?.substring(4) || "",
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", message: "Error in server" });
+  }
 });
 
-viewsRouter.get('/carts/:cid', async (req, res, next) => {
-    try {
-        const {cid} = req.params;
-        const cart = await cartService.get(cid);
-        res.render('cart', {cart});
-    } catch (error) {
-        next(error);
-    }
+viewsRouter.get("/products/:pid", async (req, res, next) => {
+  try {
+    const { pid } = req.params;
+    const product = await ProductModel.findById(pid);
+    const productSimplificado = {
+      _id: product._id.toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      thumbnail: product.thumbnail,
+      code: product.code,
+      stock: product.stock,
+      category: product.category,
+    };
+
+    console.log(productSimplificado);
+    res.render("product", { product: productSimplificado });
+  } catch (error) {
+    next(error);
+  }
+});
+
+viewsRouter.get("/carts/:cid", async (req, res, next) => {
+  try {
+    const { cid } = req.params;
+    const cart = await cartService.get(cid);
+
+    const simplifiedCart = cart.products.map((item) => {
+      return {
+        title: item.product.title,
+        price: item.product.price,
+        quantity: item.quantity,
+      };
+    });
+    console.log(simplifiedCart);
+    res.render("cart", { cart: simplifiedCart });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = viewsRouter;
--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,50 @@
 ////--------------------- MONGO ---------------------
 
-const {connect} = require("mongoose");
+const { connect } = require("mongoose");
 async function connectMongo() {
-    try {
-        await connect("mongodb+srv://jeanpierrecarrey:09lcQ3OehxvKzocQ@backendcoder.nkbcjia.mongodb.net/ecommerce?retryWrites=true&w=majority");
-        console.log("plug to mongo!");
-    } catch (e) {
-        console.log(e);
-        throw "can not connect to the db";
-    }
+  try {
+    await connect(
+      "mongodb+srv://jeanpierrecarrey:09lcQ3OehxvKzocQ@backendcoder.nkbcjia.mongodb.net/ecommerce?retryWrites=true&w=majority"
+    );
+    console.log("plug to mongo!");
+    /* let student = await CartModel.find({}).populate("products.product");
+    console.log(JSON.stringify(student, null, 2)); */
+  } catch (e) {
+    console.log(e);
+    throw "can not connect to the db";
+  }
 }
 
 exports.connectMongo = connectMongo;
 
 //--------------------- SOCKET ---------------------
-const {Server} = require('socket.io');
-const {ChatModel} = require("./DAO/models/chats.model.js");
+const { Server } = require("socket.io");
+const { ChatModel } = require("./DAO/models/chats.model.js");
 const { ProductService } = require("./services/products.service.js");
+const { CartModel } = require("./DAO/models/carts.model.js");
 
-function connectSocket(httpServer){
-    const socketServer = new Server(httpServer);
+function connectSocket(httpServer) {
+  const socketServer = new Server(httpServer);
 
-    socketServer.on('connection', (socket) => {
-        console.log('New user connected');
+  socketServer.on("connection", (socket) => {
+    console.log("New user connected");
 
-        socket.on('addProduct', async entries => {
-        const product = await ProductService.createOne(entries);
-        socketServer.emit('addedProduct', product)
-        })
-
-        socket.on('deleteProduct', async id => {
-        await ProductService.deleteOne(id);
-        socketServer.emit('deletedProduct', id)
-        })
-
-        socket.on('msg_front_to_back', async (msg) => {
-            const msgCreated = await ChatModel.create(msg);
-            const messages = await ChatModel.find({});
-            socketServer.emit('msg_back_to_front', messages);
-        });
+    socket.on("addProduct", async (entries) => {
+      const product = await ProductService.createOne(entries);
+      socketServer.emit("addedProduct", product);
     });
+
+    socket.on("deleteProduct", async (id) => {
+      await ProductService.deleteOne(id);
+      socketServer.emit("deletedProduct", id);
+    });
+
+    socket.on("msg_front_to_back", async (msg) => {
+      const msgCreated = await ChatModel.create(msg);
+      const messages = await ChatModel.find({});
+      socketServer.emit("msg_back_to_front", messages);
+    });
+  });
 }
 
 exports.connectSocket = connectSocket;
